cd888dfee024b00dec951bc3c4674703b080a7b0
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2024-01-13 13:49:50 -0300

    Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace suave.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da Categoria")]
+        [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
+        
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da Categoria")]
+        [Display(Name = "Nome")]       
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
